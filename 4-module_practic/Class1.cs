6e89f4badae62880c655d974ee5510518a60ad2e
AidynbekZh <aidinbek467@gmail.com>
2023-10-23 17:00:58 +0600

    Добавьте файлы проекта.

---
+++ 4-module_practic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_module_practic
+{
+    public class Class1
+    {
+    }
+}
